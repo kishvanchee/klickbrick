b89ef2fd4a03d740fcdf859ea3ff7b0b38cbc225
Kishore Vancheeshwaran <kishore.vancheeshwaran@gmail.com>
2021-05-19 21:49:16 +0530

    add basic unit test

--- tests/test_klickbrick.py
+++ tests/test_klickbrick.py
@@ -1,5 +1,9 @@
 from klickbrick import __version__
+import unittest
+class TestKlickbrick(unittest.TestCase):
 
 
 def test_version():
     assert __version__ == '0.1.0'
+    def test_version(self):
+        assert __version__ == '0.1.0'
